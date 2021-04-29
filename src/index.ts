fca04db29b27eaad1d06c6d05b89c8f628df8fac
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-04-29 12:02:30 +0800

    feat: add necessary functions for v1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-export default function add(a: number, b: number): number {
-  if (process.env.NODE_ENV !== 'production') {
-    console.log('This code would not appear on production builds');
-  }
-  return a + b;
-}
+import { parse, stringify } from 'uuid';
+
+export const encodeUint8Array = (uint8Array: Uint8Array): string => (
+  Buffer.from(uint8Array).toString('base64')
+);
+
+export const encode = (uuidv4: string): string => {
+  const uint8Array = parse(uuidv4) as Uint8Array;
+  const base64Encoded = encodeUint8Array(uint8Array);
+
+  return base64Encoded.slice(0, -2);
+};
+
+export const decodeBase64 = (base64: string): Uint8Array => (
+  new Uint8Array(Buffer.from(base64, 'base64'))
+);
+
+export const decode = (base64Encoded: string): string => {
+  const base64 = `${base64Encoded}==`;
+  const uint8Array = decodeBase64(base64);
+
+  return stringify(uint8Array);
+};
